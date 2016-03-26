5a1388f95ae5b1ba4ab0ac745ba4bfb29eeb22bd
ZaL <z4ls@live.com>
2016-03-26 20:17:27 +0300

    add routes to scenes

--- src/AppSceneV.js
+++ src/AppSceneV.js
@@ -3,18 +3,22 @@ import { StatusBar, Navigator, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { loginUserByToken } from './actions/Auth/login';
 import { Scene, TabBar, Modal, Schema, Actions, Switch, Router } from 'react-native-router-flux';
+import Home from './containers/Home';
 import Login from './containers/Auth/Login';
 import Register from './containers/Auth/Register';
-import Medias from './containers/Media/Medias';
-import Media from './containers/Media/Media';
 import User from './containers/User/User';
-import UserScene from './components/User/UserScene';
-import MediaComments from './containers/Media/MediaComments';
-import MediaFavorites from './containers/Media/MediaFavorites';
 import UserFavorites from './containers/User/UserFavorites';
 import UserDownloads from './containers/User/UserDownloads';
-import Home from './containers/Home';
-import CaptureMedia from './containers/Media/CaptureMedia';
+import Media from './containers/Media/Media';
+import Medias from './containers/Media/Medias';
+import MediaComments from './containers/Media/MediaComments';
+import MediaFavorites from './containers/Media/MediaFavorites';
+import MediaDownloads from './containers/Media/MediaDownloads';
+import MediaCapture from './containers/Media/MediaCapture';
+import UserMedias from './containers/User/UserMedias';
+import Followers from './containers/User/Followers';
+import Followings from './containers/User/Followings';
+import Settings from './containers/Settings';
 import TabIcon from './components/TabIcon';
 import LoginDialog from './components/LoginDialog';
 
@@ -63,20 +67,24 @@ const Scenes = Actions.create(
                barButtonTextStyle={{ fontSize:17, color:'white' }}
                style={styles.container}
         >
-          <Scene key="mediasScene"  component={Medias} rightTitle="+" onRight={()=>Actions.captureMedia()}   />
+          <Scene key="mediasScene"  component={Medias} rightTitle="+" onRight={()=>Actions.mediaCapture()}   />
           <Scene key="mediaScene" component={Media} />
-          <Scene key="userScene" component={User} />
           <Scene key="mediaCommentsScene" component={MediaComments} />
-          <Scene key="userEntityScene" component={User} />
+          <Scene key="mediaFavoritesScene" component={MediaFavorites} />
+          <Scene key="mediaDownloadsScene" component={MediaDownloads} />
+          <Scene key="userMediasScene" component={UserMedias} />
+          <Scene key="followersScene" component={Followers} />
+          <Scene key="followingsScene" component={Followings} />
+          <Scene key="userScene" component={User} />
+
         </Scene>
 
         <Scene key="home" hideNavBar={true} component={Home} icon={TabIcon} selectedTabIcon="ion|ios-home" tabIcon="ion|ios-home-outline" />
 
       </Scene>
 
-      <Scene key="captureMedia" hideNavBar={true} component={CaptureMedia}  />
+      <Scene key="mediaCapture" hideNavBar={true} component={MediaCapture}  />
       <Scene key="loginDialog"  hideNavBar={true}  component={LoginDialog} />
-
       <Scene key="login" hideNavBar={true} component={Login}  />
       <Scene key="register" component={Register} hideNavBar={true} title="تسجيل الدخول"   />
 
